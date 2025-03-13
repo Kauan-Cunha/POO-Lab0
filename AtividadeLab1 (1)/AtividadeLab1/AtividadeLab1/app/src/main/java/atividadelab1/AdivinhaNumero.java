000066fff0a34c7979c8081c6408210a4ce02cd5
Kauan Cunha da Silva <240030@dac.unicamp.br>
2025-03-13 12:24:00 -0300

    Versão final do primeiro lab

--- AtividadeLab1 (1)/AtividadeLab1/AtividadeLab1/app/src/main/java/atividadelab1/AdivinhaNumero.java
+++ AtividadeLab1 (1)/AtividadeLab1/AtividadeLab1/app/src/main/java/atividadelab1/AdivinhaNumero.java
@@ -5,12 +5,33 @@ import java.util.Scanner;
 //IMPLEMENTE O EXERCICIO ABAIXO
 class AdivinhaNumero {
     int numeroSecreto;
-
+    int numTentativas;
+    int palpiteAtual;
     public AdivinhaNumero() {
         Random random = new Random();
-        numeroSecreto = random.nextInt(100) + 1; // Número entre 1 e 100
-        
+        numeroSecreto = random.nextInt(100) + 1;// Número entre 1 e 100
+        numTentativas = 0;
     }
+    public void fazPalpite(){
+        Scanner leitor = new Scanner(System.in);
+        int palpite = leitor.nextInt();
 
+        palpiteAtual = palpite;
+        numTentativas +=1;
+    }
+    public boolean verificaPalpite(){
+        if(palpiteAtual == numeroSecreto){
+            System.out.println("Palpite Correto");
+            return true;
+        } else if (palpiteAtual > numeroSecreto){
+            System.out.println("Palpite maior que número secreto");
+            return false;
+        }
+        System.out.println("Palpite menor que número secreto");
+        return false;
+    }
+    public void imprimeTentativas(){
+        System.out.println(numTentativas);
+    }
     
 }
